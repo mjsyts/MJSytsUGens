1213699d71d1c8add4e2b4f7ee8ed35e67561eb2
Josiah Sytsma <77947318+mjsyts@users.noreply.github.com>
2024-04-17 08:28:08 -0400

    Update TBFF.cpp
    Fix rate handling of parameters

--- MJSytsUGens/plugins/TBFF/TBFF.cpp
+++ MJSytsUGens/plugins/TBFF/TBFF.cpp
@@ -1,18 +1,11 @@
-// PluginTBFF.hpp
 // M. Josiah Sytsma (sytsma.music@icloud.com)
 
 #include "TBFF.h"
 #include "SC_PlugIn.h"
 #include "SC_PlugIn.hpp"
 
-static InterfaceTable* ft;
-
-TBFF::TBFF() {
+TBFF::TBFF() : omega(M_PI * sampleDur()) {
     reset();
-    
-    m_Freq = in0(FREQ_IN);
-    m_Res = in0(RQ_IN);
-    
     set_calc_function<TBFF, &TBFF::next>();
 }
 
@@ -27,13 +20,12 @@ void TBFF::next(int inNumSamples) {
     SlopeSignal<float> slopedFreq = makeSlope(in0(FREQ_IN), m_Freq);
     SlopeSignal<float> slopedRes = makeSlope(in0(RQ_IN), m_Res);
     
-    const float freq = slopedFreq.consume();
-    const float res = slopedRes.consume();
-    
     //output
     float* outBuf = out(0);
     
     for (int i = 0; i < inNumSamples; i++) {
+        const float freq = slopedFreq.consume();
+        const float res = slopedRes.consume();
         oversample.upsample(xn[i]);
         
         for (int j = 0; j < 2; j++) {
@@ -49,14 +41,14 @@ void TBFF::next(int inNumSamples) {
     m_Res = slopedRes.value;
 }
 
-float TBFF::processSample(const float inSample, const float freq, const float res){
+float TBFF::processSample(const float& inSample, float freq, float res){
     float y0, y1, y2, y3, y4;
     
-    const float k = (1.f - sc_clip(res, 0.0, 1.0)) * 20.f;
+    const float k = (1 - sc_clip(res, 0.0, 1.0)) * 25;
+    
     const float a = 1.f + 0.5 * k;
     
-    float wc = M_PI * freq * sampleDur();
-    
+    const float wc = omega * freq;
     const float wc2 = wc * wc;
     const float wc3 = wc2 * wc;
     const float wc4 = wc3 * wc;
@@ -71,10 +63,8 @@ float TBFF::processSample(const float inSample, const float freq, const float re
     y4 = (g * inSample + s) / (1 + g * k);
     
     y0 = inSample - k * y4;
-    //saturate output
-//    y0 = tanh(y0);
-    y0 = sc_clip2(y0, 3.f);
-    y0 *= (27 + y0 * y0) / (27 + 9 * y0 * y0 );
+
+    y0 = Pade::tanh(y0);
     
     y1 = (y0 * (2*wc + 12*wc2 + 20*wc + 8*wc4) + z0*(1 + 6*wc + 10*wc2 + 4*wc3) + z1*(2*wc + 8*wc2 + 6*wc3) + z2*(2*wc2 + 4*wc3) + z3 * 2 * wc3) * b;
     y2 = (y0*(2*wc2 + 8*wc3 + 6*wc4) + z0*(wc + 4*wc + 3*wc3) + z1*(1 + 6*wc + 11*wc + 6*wc3) + z2*(wc + 4*wc2 + 4*wc3) + z3*(wc2 + 2*wc3)) * b;
@@ -88,10 +78,5 @@ float TBFF::processSample(const float inSample, const float freq, const float re
     z3 += 2 * wc * (y3 - 2 * y4);
     
     //output
-    return y4 * a;
+    return Pade::tanh(y4 * a);
 };
-
-PluginLoad(TBFFUGens) {
-    ft = inTable;
-    registerUnit<TBFF>(ft, "TBFF", false);
-}
