11ce29dcae4d5449956b111cfa2d467bf7f8f8d5
Josiah Sytsma <77947318+mjsyts@users.noreply.github.com>
2024-04-17 08:33:17 -0400

    Update TBFF.h
    Include PadeApprox.h

--- MJSytsUGens/plugins/TBFF/TBFF.h
+++ MJSytsUGens/plugins/TBFF/TBFF.h
@@ -1,9 +1,12 @@
 #include "SC_PlugIn.hpp"
 #include "Oversampling.h"
+#include "PadeApprox.h"
 
 class TBFF : public SCUnit {
 private:
     enum Inputs { SIG_IN, FREQ_IN, RQ_IN, NUM_INS };
+    
+    const float omega;
     
     float m_Freq, m_Res;
     
@@ -13,12 +16,12 @@ private:
     float* osBuffer;
     
     // Calc functions
-    void next(int nSamples);
+    void next(int inNumSamples);
     
     //reset integrator states
     void reset();
     
-    float processSample(const float inSample, const float freq, const float res);
+    float processSample(const float& inSample, float freq, float res);
 public:
     TBFF();
     Oversampling<2> oversample; //fixed 2x oversample
